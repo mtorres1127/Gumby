96ee790f12dc37eec1992d7b2bc87fe1311621af
mtorres1127 <87192726+mtorres1127@users.noreply.github.com>
2022-07-21 17:03:33 -0500

    formatted

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    body: Column(
-      children: [
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+        const Text('Climb On'),
         Image.asset(
-            "assets/scott-osborn-unsplash.jpg",
+          "assets/scott-osborn-unsplash.jpg",
           width: double.infinity,
-          fit: BoxFit.fitHeight,
+          fit: BoxFit.cover,
         ),
-         Text('Hello Climbers'),
-      ]
-    ),
-
-  ),
-));
-
-
+      ]),
+    );
+  }
+}
